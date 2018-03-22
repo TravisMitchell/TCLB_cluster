e1df9d349a2f833a18f22057c94fb7a44d602ed6
TravisMitchell <travis.mitchell@uqconnect.edu.au>
2018-03-22 10:01:49 +0800

    + gnu settings, currently works for pf_velocity models (breaks on base d2q9 model...)

--- cluster/magnus.bash
+++ cluster/magnus.bash
@@ -10,10 +10,11 @@ MAINQ_DEF="workq"
 MAINQ_ASK="no"
 RUN_GPU_DEF="n"
 RUN_GPU_ASK="no"
-MODULES_RUN_DEF="PrgEnv-cray"
-MODULES_UNLOAD_DEF="PrgEnv-cray"
+#MODULES_RUN_DEF="PrgEnv-cray cce"
+MODULES_RUN_DEF="PrgEnv-gnu gcc"
+MODULES_UNLOAD_DEF="PrgEnv-cray cce"
 MODULES_UNLOAD_ASK="no"
-CONFOPT_DEF="--disable-cuda"
+CONFOPT_DEF="--disable-cuda --disable-graphics CXX=CC CC=cc"
 MAX_UNITS_PER_NODE_DEF=24
 CORES_PER_UNIT_DEF=1
 CORES_PER_UNIT_FULL_DEF=1
@@ -24,5 +25,5 @@ MODULES_ADD_DEF=""
 MODULES_ADD_ASK="no"
 MODULES_RUN_ASK="no"
 
-[ -z "$RHOME" ] && RHOME=$(echo /pawsey/cle*/apps/PrgEnv-gnu/*/gcc/*/haswell/r/*/bin/R | sed 's|bin/R||' | head -n1)
+[ -z "$RHOME" ] && RHOME=$(echo /opt/R/*/bin/R | sed 's|bin/R||' | head -n1)
 RHOME_ASK=""
